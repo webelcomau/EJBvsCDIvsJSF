53fdf0a5b97c3980d2c5201e227e3f18921dd2a6
Darren Kelly <webelcomau@users.noreply.github.com>
2018-01-17 23:33:48 +1100

    Clearer docs.

--- src/java/com/webel/jsf/AbstractBackingBean.java
+++ src/java/com/webel/jsf/AbstractBackingBean.java
@@ -10,7 +10,6 @@ import com.webel.ejb.StatefulSession;
 import com.webel.ejb.StatefulViewView;
 import com.webel.ejb.StatelessEjb;
 import com.webel.ejb.StatelessInject;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
@@ -22,8 +21,6 @@ import javax.inject.Inject;
  * @author darrenkelly
  */
 abstract public class AbstractBackingBean extends All {
-
-    private static final Logger logger = Logger.getLogger(AbstractBackingBean.class.getName());
 
     private Class scopeClass;
 
@@ -98,6 +95,9 @@ abstract public class AbstractBackingBean extends All {
         myPreDestroy();
     }
 
+    /**
+     * This should usually remain false.
+     */
     static final private boolean DO_NULL_EJB_RESOURCES = false;
 
     @Inject private AppOptions appOptions;
@@ -105,9 +105,8 @@ abstract public class AbstractBackingBean extends All {
     //@Override
     protected void myPreDestroy() {
 
-        // The @Remove remove() MUST be invoked for @PreDestroy to be forced on @Stateful when
-        // @EJB used for injection, otherwise @PreDestroy only invoked by container on timeout.
-        
+        // The @Remove remove() MUST be invoked by the application for @PreDestroy to be forced on @Stateful 
+        // when @EJB is used for injection, otherwise @PreDestroy is only invoked automatically on timeout.        
         if (appOptions.isDoForceRemoveEjb()) {
             echo("WILL force remove() on @EJB statefulEjb !");
             statefulEjb.remove(); //! Usually needed because not "contextual"
@@ -115,13 +114,6 @@ abstract public class AbstractBackingBean extends All {
         else {
             echo("SKIP: WILL NOT force remove() on @EJB statefulEjb !");        
         }
-
-        // For @RequestScoped, a @Stateful EJB injected with @Inject will have its @PreDestroy
-        // called by the container.
-        //
-        // But for @ViewScoped (Mojarra or OmniFaces version) @Remove remove() MUST be invoked 
-        // for @PreDestroy to be forced on @Stateful when @Inject used for injection on the EJB,
-        // otherwise @PreDestroy only invoked by container on timeout.
         
         if (appOptions.isDoForceRemoveInject()) {
             String $f = "WILL force remove() on @Inject %s !";
@@ -137,20 +129,23 @@ abstract public class AbstractBackingBean extends All {
             // of a session bean that is a stateful session bean and declares any scope other 
             // than @Dependent, an UnsupportedOperationException is thrown.'
             
+            // The following are all NOT needed because "contextual" and BREAK THE SPEC !
             echo(String.format($f,"statefulRequest"));            
-            statefulRequest.remove();// Not needed because "contextual": BREAKS SPEC !
+            statefulRequest.remove();
             
             echo(String.format($f,"statefulViewView"));            
-            statefulViewView.remove();// Not needed because "contextual": BREAKS SPEC !
+            statefulViewView.remove();
             
             echo(String.format($f,"statefulOmniView"));            
-            statefulOmniView.remove();// Not needed because "contextual": BREAKS SPEC !            
+            statefulOmniView.remove();
             
             echo(String.format($f,"statefulSession"));            
-            statefulSession.remove();// Not needed because "contextual": BREAKS SPEC !           
+            statefulSession.remove();
             
         }
-        else {
+        else {            
+            
+            //Verbose, but makes it clear relying on container completely.
             String $f = "SKIP: WILL NOT force remove() on @Inject %s !";            
             echo(String.format($f,"statefulInject"));                        
             echo(String.format($f,"statefulDepend"));                        
@@ -160,6 +155,8 @@ abstract public class AbstractBackingBean extends All {
             echo(String.format($f,"statefulSession"));
         }
         
+        // Following for test purposes only, should usually be skipped.
+        // Not needed if the injected session beans are "destroyed" properly.
         if (DO_NULL_EJB_RESOURCES) {            
             
             echo("force null all EJBs");
